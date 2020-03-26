b11ac7dbb859fa9674adb504b8fd36cc9e8a70e8
elvisonobo <rapknowlogy@gmail.com>
2020-03-26 11:56:39 +0100

    added test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+// create dummy data to use for GET route tests
+const todos = [{
+    text: 'First todo'
+},{
+    text: 'second todo'
+}];
+
 // delete all items in the DB before running our test
 beforeEach((done)=>{
     // passing an empty object makes it delete everything from DB
-    Todo.deleteMany({}).then(()=>done());
+    Todo.deleteMany({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 // describe method groups tests with similar routes/purposes
@@ -28,7 +37,7 @@ describe('POST /todos', ()=>{
                 }
 
                 // get all the todos from DB and test to assert the right todo was saved
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -47,9 +56,21 @@ describe('POST /todos', ()=>{
                 }
 
                 Todo.find().then((todos)=>{
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
     });
+});
+
+describe('GET /todos', ()=>{
+    it('should get all todos', (done)=>{
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    })
 });
